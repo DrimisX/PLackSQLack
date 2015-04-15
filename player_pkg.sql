67aa57c144fd8f0f6049be869f56c2c0d3952c1c
Ashika Shallow <slwashika@gmail.com>
2015-04-15 16:43:22 -0400

    Added change_password procedure

--- player_pkg.sql
+++ player_pkg.sql
@@ -1,14 +1,19 @@
 -- Package specifications
 
 CREATE OR REPLACE PACKAGE player_package IS
+  -- Create player procedure
   PROCEDURE create_player(
     p_account player.accountName%TYPE, p_password player.password%TYPE,
     p_fName player.firstName%TYPE, p_lName player.lastName%TYPE,
     p_email player.email%TYPE);
+    
+  -- Change password procedure
+  PROCEDURE change_password(p_account player.accountName%TYPE, p_password player.password%TYPE);
 END player_package;
 
 -- Package body
 CREATE OR REPLACE PACKAGE BODY player_package IS
+  -- Create player procedure
   PROCEDURE create_player(
       p_account player.accountName%TYPE, p_password player.password%TYPE,
       p_fName player.firstName%TYPE, p_lName player.lastName%TYPE,
@@ -20,4 +25,19 @@ CREATE OR REPLACE PACKAGE BODY player_package IS
       WHEN DUP_VAL_ON_INDEX THEN
         RAISE_APPLICATION_ERROR(-20001, 'That user already exists');
   END create_player;
+  
+  -- Change password procedure
+  PROCEDURE change_password(p_account player.accountName%TYPE, 
+                            p_password player.password%TYPE) 
+  IS
+  p_first player.accountName%TYPE;
+  BEGIN
+    SELECT firstName INTO p_first FROM
+      Player WHERE accountName = p_account;
+    UPDATE Player SET password = p_password
+       WHERE accountName = p_account;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        RAISE_APPLICATION_ERROR(-20002, 'That user does not exists');
+  END change_password;
 END player_package;
