a0892dff787db911588007ee5b2701546222be07
Ashika Shallow <slwashika@gmail.com>
2015-04-15 16:01:34 -0400

    Added create_player procedure

--- player_pkg.sql
+++ player_pkg.sql
@@ -1,19 +1,23 @@
 -- Package specifications
 
 CREATE OR REPLACE PACKAGE player_package IS
-  TYPE player_type IS RECORD(f_name player.firstName%TYPE, 
-                         l_name player.lastName%TYPE,
-                         pl_email player.email%TYPE);
-  -- Function does not return player password  
-  FUNCTION get_player(p_account player.accountName%TYPE) RETURN player_type;
   PROCEDURE create_player(
     p_account player.accountName%TYPE, p_password player.password%TYPE,
     p_fName player.firstName%TYPE, p_lName player.lastName%TYPE,
     p_email player.email%TYPE);
-  PROCEDURE update_player(p_account player.accountName%TYPE, p_fName player.firstName%TYPE,
-                          p_lName player.lastName%TYPE, p_email player.email%TYPE);
-  PROCEDURE remove_player(p_account player.accountName%TYPE);
-  PROCEDURE change_password(p_account player.accountName%TYPE);
 END player_package;
 
 -- Package body
+CREATE OR REPLACE PACKAGE BODY player_package IS
+  PROCEDURE create_player(
+      p_account player.accountName%TYPE, p_password player.password%TYPE,
+      p_fName player.firstName%TYPE, p_lName player.lastName%TYPE,
+      p_email player.email%TYPE) IS    
+  BEGIN   
+    INSERT INTO Player VALUES
+      (p_account, p_password, p_fName, p_lName, p_email);
+    EXCEPTION
+      WHEN DUP_VAL_ON_INDEX THEN
+        RAISE_APPLICATION_ERROR(-20001, 'That user already exists');
+  END create_player;
+END player_package;
