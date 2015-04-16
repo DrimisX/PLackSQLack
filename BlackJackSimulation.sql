9a2e8cbdb58f1210ec2b192dfd89739e282b7fc8
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2015-04-16 14:28:26 -0400

    Renamed to BlackJackSimulation.sql
    Removed unnecessary variables.

--- BlackJackSimulation.sql
+++ BlackJackSimulation.sql
@@ -9,37 +9,6 @@ DECLARE
 	P2_ACCOUNT_NAME := 'Jasmyn234';
 	P3_ACCOUNT_NAME := 'Dylan365';
 
-	-- Global Variables
-	gameID := '';
-
-	v_player_count INT;
-	v_verbose_messaging BOOLEAN;
-
-	v_p1_account_name := '';
-	v_p2_account_name := '';
-	v_p3_account_name := '';
-	v_p4_account_name := '';
-
-	v_current_player INT;
-	v_next_player BOOLEAN;
-
-	v_dealer_hand VARCHAR := '';
-	v_dealer_hand_val INT := 0;
-
-	v_p1_account_name VARCHAR := '';
-
-	v_p1_hand VARCHAR := '';
-	v_p2_hand VARCHAR := '';
-	v_p3_hand VARCHAR := '';
-	v_p4_hand VARCHAR := '';
-
-	v_p1_hand_val INT := 0;
-	v_p2_hand_val INT := 0;
-	v_p3_hand_val INT := 0;
-	v_p4_hand_val INT := 0;
-
-
-
 BEGIN
 
 	-- Pass v_player count to Init_Proc(), Update gameID, update v_verbose_messaging and v_player_count
