a64f7871af0d595275ed0964b642b7914ce0111d
DrimisX <github@dylanweb.ca>
2015-04-16 16:34:28 -0400

    Update deck_package.sql

--- deck_package.sql
+++ deck_package.sql
@@ -1,16 +1,55 @@
-/* Created By Dylan Huculak */
+CREATE OR REPLACE PACKAGE deck_pkg IS  
+  
+  -- CONSTANTS --
+  
+  -- VARIABLES --
+  v_hands_to_deal;				-- Total number of players including deadler
+  v_current_player INT;		-- Current player being dealt to
+	v_next_player BOOLEAN;	-- Is there is another player this round
 
-CREATE OR REPLACE PACKAGE deck_package IS  
-  -- PROCEDURES
+	v_dealer_hand VARCHAR2;	-- A string representing the dealer's cards
+	v_dealer_hand_val INT;	-- Dealer's hand total value
+
+	v_p1_account_name VARCHAR2;
+
+	v_p1_hand VARCHAR2;
+	v_p2_hand VARCHAR2;
+	v_p3_hand VARCHAR2;
+	v_p4_hand VARCHAR2;
+
+	v_p1_hand_val INT;
+	v_p2_hand_val INT;
+	v_p3_hand_val INT;
+	v_p4_hand_val INT;
+	
+	v_round_result VARCHAR2;
+	
+	-- PROCEDURES --
   PROCEDURE shuffle_deck;
   
-  -- FUNCTIONS
+  -- FUNCTIONS --
   FUNCTION get_card_value;
-  -- Include other functions/procedures --
-END deck_package;
+  
+END deck_pkg;
 
 -- DECK PACKAGE
-CREATE OR REPLACE PACKAGE BODY deck_package IS
+CREATE OR REPLACE PACKAGE BODY deck_pkg IS
+
+	-- ***CONSTANTS*** --
+	
+	-- ***VARIABLES*** --
+	v_dealer_hand_val := 0;
+	v_p1_hand_val := 0;
+	v_p2_hand_val := 0;
+	v_p3_hand_val := 0;
+	v_p4_hand_val := 0;
+	
+	v_hands_to_deal := game_pkg.v_player_count + 1;
+	
+	
+	-- ***PROCEDURES*** --
+	
+	-- ***FUNCTIONS*** --
   
   -- PROCEDURE shuffle_deck clears the existing ShuffledDeck table
   --  reads all rows from the Deck table (ordering by random), 
@@ -28,16 +67,17 @@ CREATE OR REPLACE PACKAGE BODY deck_package IS
   -- It will return 10 for J, Q, K, and either 1 or 11 for A, 
   --	depending on the total value of the hand it is being added to.
   -- It requires two arguments: 
-  --	The first is a VARCHAR representing the face value
+  --	The first is a VARCHAR2 representing the face value
   --	The second is a NUMBER representing the existing hand value
   FUNCTION get_card_value
-	  ( face_value IN VARCHAR		-- The face value of the card
+	  ( face_value IN VARCHAR2		-- The face value of the card
 		  hand_value IN NUMBER )	-- The hand value before this card is added
 	  RETURN number 			-- Return type
 	  IS
 	
   DECLARE
 	  value NUMBER;				-- The numerical value to be returned
+	  err_text VARCHAR2;
 	
   BEGIN
 	  -- Assign proper value based on face_value
@@ -55,7 +95,7 @@ CREATE OR REPLACE PACKAGE BODY deck_package IS
 	  			value := 1;
   			END IF;
 		-- If face_value is not a letter, 
-	  	-- convert from VARCHAR to NUMBER
+	  	-- convert from VARCHAR2 to NUMBER
   		ELSE
 		  	TO_NUMBER(face_value)
 	  END
@@ -64,8 +104,39 @@ CREATE OR REPLACE PACKAGE BODY deck_package IS
   EXCEPTION
 	  -- Miscellaneous exception handler
 	  WHEN OTHERS THEN
-		  DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLCODE)||
-	  		": ERROR IN FUNCTION get_card_value - " || SQLERRM);
-END;
+		  err_text = ": ERROR IN FUNCTION get_card_value - " || SQLERRM;
+		  DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLCODE)|| errText);
+	  	log_error(SQLCODE, errText);
+  END;
   
-END deck_package;
+  -- PROCEDURE deal_cards deals out the starting hand values to each player.
+  -- It deals one card to each player and the dealer, then a second card to each.
+  -- The dealer's up card is recorded and shown to the players.
+  PROCEDURE deal_cards IS
+  v_deal_result VARCHAR2;
+  v_play_result VARCHAR2;
+  v_cur_player VARCHAR2;
+  	BEGIN
+  		FOR i IN 1..2
+  		LOOP
+  			FOR j IN 1..hands_to_deal
+  			LOOP
+  			
+  			
+  				IF j < hands_to_deal
+  					v_cur_player = "Player " || j
+  				ELSE
+  					v_cur_player = "Dealer"
+  				END IF;
+  				v_deal_result = v_deal_result || v_cur_player || " receives a " ||  
+  				
+  	END
+  -- FUNCTION deal_card deals out one card by reading from the ShuffledDeck table,
+  -- and returns the card's face value.
+  	-- CODE HERE
+  	
+  -- FUNCTION card_to_string takes a card (i.e. '2H'), and outputs
+  -- a string for better output to the user (2 of Hearts).
+  	-- CODE HERE
+  
+END deck_pkg;
