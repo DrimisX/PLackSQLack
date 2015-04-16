64bb130b409bc57b336010834e8b5bd303e9c050
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2015-04-16 16:31:59 -0400

    Restructured BlackJackSimulation.sql
    Calling of Procedures and Functions Simplified

--- BlackJackSimulation.sql
+++ BlackJackSimulation.sql
@@ -2,7 +2,7 @@
 
 DECLARE
 	
-	-- CONSTANTS
+	-- Constant Declaration for Simulation
 	PLAYER_COUNT INT := 3;
 	VERBOSE_MESSAGE BOOLEAN := true;
 	P1_ACCOUNT_NAME := 'Ashika123';
@@ -11,22 +11,16 @@ DECLARE
 
 BEGIN
 
-	-- Pass v_player count to Init_Proc(), Update gameID, update v_verbose_messaging and v_player_count
-	game_pkg.Init_Proc(PLAYER_COUNT, VERBOSE_MESSAGE);
+	-- Create Game, Set Up Player Count and Whether or not Messaging is Verbose
+	game_pkg.init_proc(PLAYER_COUNT, VERBOSE_MESSAGE);
 
-	if resumegame = false-- Decide Players, Update v_player_count
-		player_pkg.AddPlayers();
-		deck_pkg.ShuffleDeck();
-		deck_pkg.DealCards();
+	-- Add Players to Game
+	game_pkg.add_players(P1_ACCOUNT_NAME, P2_ACCOUNT_NAME, P3_ACCOUNT_NAME)
 
-	-- Evaluates current turn, loops through players and dealer
-	WHILE v_next_player
-		LOOP
-			deck_pkg.Turns();
-		END LOOP
+	-- Create Shuffled Deck
+	deck_pkg.shuffle_deck();
 
-	-- Output Winner to screen with Stats
-	output_pkg.OutputResults();
+	DBMS_OUTPUT.PUT_LINE(deck_pkg.deal_game);
 
 EXCEPTION
 END;
