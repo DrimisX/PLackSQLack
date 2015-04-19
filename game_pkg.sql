206f632eed276984dc633389b2eca1e99b5990c7
DrimisX <github@dylanweb.ca>
2015-04-19 13:54:46 -0400

    Changed table/column names to match new create_insert, fixed some variable name errors

--- game_pkg.sql
+++ game_pkg.sql
@@ -1,24 +1,25 @@
 -- Package Specification
 CREATE OR REPLACE PACKAGE game_pkg AS
 
-  v_game_id Game.gameID%TYPE;
+  v_game_id games.game_id%TYPE;
+  v_err_text error_logs.err_msg%TYPE;
   
   v_player_count INT;
   v_verbose_messaging BOOLEAN;
   
-  v_p1_account_name Game.accountName%TYPE;
-  v_p2_account_name Game.accountName%TYPE;
-  v_p3_account_name Game.accountName%TYPE;
-  v_p4_account_name Game.accountName%TYPE;
+  v_p1_account_name games.account_name%TYPE;
+  v_p2_account_name games.account_name%TYPE;
+  v_p3_account_name games.account_name%TYPE;
+  v_p4_account_name games.account_name%TYPE;
 
   -- PROCEDURES --
   PROCEDURE init_proc (p_player_count INT DEFAULT 1, p_verbose_messaging BOOLEAN DEFAULT false);
-  PROCEDURE log_error (p_err_code GameErrorLog.errorCode%TYPE, p_err_msg GameErrorLog.errorMessage%TYPE);
+  PROCEDURE log_error (p_err_code error_logs.err_code%TYPE, p_err_msg error_logs.err_msg%TYPE);
   PROCEDURE add_players (
-  	p_p1_account_name Game.accountName%TYPE,
-  	p_p2_account_name Game.accountName%TYPE DEFAULT NULL,
-  	p_p3_account_name Game.accountName%TYPE DEFAULT NULL,
-  	p_p4_account_name Game.accountName%TYPE DEFAULT NULL
+  	p_p1_account_name games.account_name%TYPE,
+  	p_p2_account_name games.account_name%TYPE DEFAULT NULL,
+  	p_p3_account_name games.account_name%TYPE DEFAULT NULL,
+  	p_p4_account_name games.account_name%TYPE DEFAULT NULL
   	);
 
 END game_pkg;
@@ -29,32 +30,32 @@ CREATE OR REPLACE PACKAGE BODY game_pkg AS
   -- Create Game, Set v_player_count and v_verbose_messaging according to parameter
   PROCEDURE init_proc (p_player_count INT DEFAULT 1, p_verbose_messaging BOOLEAN DEFAULT false) IS
 		
-  v_game_date Game.gameDate%TYPE;
+  v_game_date games.game_date%TYPE;
 
   BEGIN		
     -- Set v_game_date to Current Date/Time
 	v_game_date := SYSDATE;
 
 	-- Create new game in Game Table
-	INSERT INTO Game(date) VALUES (v_game_date);
+	INSERT INTO games (date) VALUES (v_game_date);
 		
 	-- Set Variables
-	SELECT gameID INTO v_game_id FROM Game WHERE gameDate;
+	SELECT game_id INTO v_game_id FROM games WHERE game_date;
 	v_player_count := p_player_count;
 
   EXCEPTION
-	err_text := "ERROR IN PROCEDURE init_proc - " || SQLERRM;
-	DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLCODE)|| errText);
-  	log_error(SQLCODE, errText);
+	v_err_text := "ERROR IN PROCEDURE init_proc - " || SQLERRM;
+	DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLCODE)|| v_err_text);
+  	log_error(SQLCODE, v_err_text);
 	
   END init_proc;
 
   -- Procedure to Call when an Error Occurs
   -- Log Error in GameErrorLog Table
-  PROCEDURE log_error (p_err_code GameErrorLog.errorCode%TYPE, p_err_msg GameErrorLog.errorMessage%TYPE) IS					
+  PROCEDURE log_error (p_err_code error_logs.err_code%TYPE, p_err_msg error_logs.err_msg%TYPE) IS					
   BEGIN
 	-- Insert information into GameErrorLog Table
-	INSERT INTO GameErrorLog(errorCode, errorMessage, gameID)
+	INSERT INTO error_logs (err_code, error_msg, game_id)
 	  VALUES (p_err_code, p_err_msg, v_game_id);
 
   EXCEPTION
@@ -64,10 +65,10 @@ CREATE OR REPLACE PACKAGE BODY game_pkg AS
   END log_error;
 
   -- Add Players to PlayerGame Table based on the Player Count
-  PROCEDURE add_players (p_p1_account_name Game.accountName%TYPE, 
-                       p_p2_account_name Game.accountName%TYPE DEFAULT NULL, 
-					   p_p3_account_name Game.accountName%TYPE DEFAULT NULL, 
-					   p_p4_account_name Game.accountName%TYPE DEFAULT NULL) IS
+  PROCEDURE add_players (p_p1_account_name games.account_name%TYPE, 
+                       p_p2_account_name games.account_name%TYPE DEFAULT NULL, 
+					   p_p3_account_name games.account_name%TYPE DEFAULT NULL, 
+					   p_p4_account_name games.account_name%TYPE DEFAULT NULL) IS
 	
     v_current_insert VARCHAR2;
   BEGIN
@@ -80,15 +81,15 @@ CREATE OR REPLACE PACKAGE BODY game_pkg AS
 	    WHEN 4 THEN v_current_user := p_p4_account_name
 	  END CASE;
 	  -- Insert Player into PlayerGame
-	  INSERT INTO PlayerGame (gameID, accountName, playerPos)
+	  INSERT INTO player_games (game_id, account_name, player_pos)
 	    VALUES (v_game_id, v_current_insert, i)
 	END LOOP;
 
   EXCEPTION
 	-- Miscellaneous exception handler
 	WHEN OTHERS THEN
-	  err_text := "ERROR IN PROCEDURE add_players - " || SQLERRM;
-	  DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLCODE)|| errText);
-  	  log_error(SQLCODE, errText);
+	  v_err_text := "ERROR IN PROCEDURE add_players - " || SQLERRM;
+	  DBMS_OUTPUT.PUT_LINE(TO_CHAR(SQLCODE)|| v_err_text);
+  	  log_error(SQLCODE, v_err_text);
   END add_players;
 END game_pkg;
